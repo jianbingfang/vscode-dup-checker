90384ed7f7523921d8db8a93dff9e9cf03e9d61c
Jesse Fang <jifang@microsoft.com>
2017-12-18 18:01:06 +0800

    [Feature] Support regex match

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Check duplicate lines in the file content.
 `ctrl+shift+p` Commands:
 - `Check Duplicates`: Check duplicate lines in file immediately (trim empty characters only)
 - `Check Duplicates With Trim Condition`: Check duplicate lines in file (trim both empty and customer input characters)
+- `Check Duplicates With Regex Match`: Check duplicate lines in file using customer regex (trim empty characters and then compare regex matched string in each line)
--- extension.js
+++ extension.js
@@ -15,11 +15,16 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.checkDupWithTrim', function () {
+    let disposable = vscode.commands.registerCommand('extension.checkDup', function () {
         // The code you place here will be executed every time your command is executed
-        
         // Display a message box to the user
         // vscode.window.showInformationMessage(`${dupLines.length} duplicates found.`);
+        checkDup();
+    });
+
+    context.subscriptions.push(disposable);
+
+    disposable = vscode.commands.registerCommand('extension.checkDupWithTrim', function () {
         vscode.window.showInputBox({
             prompt: 'Characters to trim'
         }).then(input => {
@@ -30,25 +35,49 @@ function activate(context) {
 
     context.subscriptions.push(disposable);
 
-    disposable = vscode.commands.registerCommand('extension.checkDup', function () {
-        checkDup();
+    disposable = vscode.commands.registerCommand('extension.checkDupWithRegex', function () {
+        vscode.window.showInputBox({
+            prompt: 'RegExp to match and selet for each line'
+        }).then(input => {
+            if (input === undefined) return;
+            checkDup('', input);
+        })
     });
 
     context.subscriptions.push(disposable);
 
-    function checkDup(trimChars) {
+    function checkDup(trimChars, regex) {
         let doc = vscode.window.activeTextEditor.document;
         const text = doc.getText();
-        const lines = text.split('\r\n').map(line => _.trim(line.trim(), trimChars)).filter(line => !_.isEmpty(line));
+        let lines = text.split('\r\n').map(line => line.trim());
+
+        if (!_.isEmpty(trimChars)) {
+            lines = lines.map(line => _.trim(line, trimChars));
+        }
+
+        if (!_.isEmpty(regex)) {
+            regex = _.trim(regex, '/');
+            const re = new RegExp(regex);
+            if (!re) return vscode.window.showErrorMessage(`[Invalid Regex]: ${regex}`);
+            lines = lines.map(line => {
+                const match = re.exec(line)
+                return match ? match[match.length - 1] : '';
+            })
+        }
+
+        lines = lines.filter(line => !_.isEmpty(line));
         const dupLines = []
         lines.forEach((line, i) => {
             if (dupLines.indexOf(line) === -1 && lines.indexOf(line, i + 1) >= 0) {
                 dupLines.push(line)
             }
         });
+
+        const trimInfo = _.isEmpty(trimChars) ? '' : ` (trim: ${trimChars})`;
+        const regexInfo = _.isEmpty(regex) ? '' : ` (regex: /${regex}/)`;
         output.clear();
         output.show();
-        output.appendLine(`${dupLines.length} duplicates found${_.isEmpty(trimChars) ? '' : ' (trim: ' + trimChars + ')'}:`);
+        output.appendLine(`${dupLines.length} duplicates found${trimInfo}${regexInfo}:`);
         dupLines.forEach(line => output.appendLine(line));
     }
 }
